ec61b2c44beaac02759e9b828cbb170a6f05d923
Jacob McKenna <mcjacob61@gmail.com>
2023-12-17 16:09:43 -0900

    Added rowIndex
    - Added rowIndex for getting (x,y) coordinate
    - Added function type hint for Position List

--- Puzzle3/entity.py
+++ Puzzle3/entity.py
@@ -11,32 +11,31 @@ class Entity(object):
 
 class Number(Entity):
 
-    def __init__(self, number, positions):
+    def __init__(self, number, positions: list['Position']):
         super().__init__()
         
         self.number_ = number
-        self.positions_ = []
-        for position in positions:
-            self.positions_.append(Position(position))
-
+        self.positions_ = positions
+        
     def GetTokenValue(self):
         return self.number_
 
 class Position(object):
 
-    def __init__(self, positionValue):
-        self.positionValue_ = positionValue
+    def __init__(self, rowIndex, colIndex):
+        self.positionRowIndex_ = rowIndex
+        self.positionColumnIndex_ = colIndex
 
 class Symbol(Entity):
     
-    def __init__(self, symbol, position):
+    def __init__(self, symbol, rowIndex, position):
         self.symbol_ = ""
 
         if symbol in SYMBOLS_TO_IGNORE:
             raise Exception(f"Character cannot be an ignored character. Character given: {symbol}")
         
         self.symbol_ = symbol
-        self.position_ = Position(position)   
+        self.position_ = Position(rowIndex, position)
 
     def GetTokenValue(self):
         return self.symbol_
--- Puzzle3/main.py
+++ Puzzle3/main.py
@@ -3,51 +3,50 @@
 # 12/16/23
 
 from constants import *
-from entity import Symbol, Number
+from entity import Symbol, Number, Position
 import re 
 
-def CreateTokens(row):
+def CreateTokens(rowIndex, row, symbolTokens, numberTokens):
     position = 0
     numChars = len(row)
     currentNumber = ""
     currentNumberPositions = []
-    tokens = []
     while position < numChars:
         currentChar = row[position]
 
         if currentChar.isdigit():
             currentNumber += currentChar
-            currentNumberPositions.append(position)
+            currentNumberPositions.append(Position(rowIndex, position))
             if row[position + 1] in SYMBOLS_TO_IGNORE or row[position + 1] in SYMBOLS:
                 # print(currentNumber)
-                tokens.append(Number(currentNumber, currentNumberPositions))
+                numberTokens.append(Number(currentNumber, currentNumberPositions))
                 currentNumber = ""
                 currentNumberPositions = []
         elif currentChar in SYMBOLS:
             # print(f"Symbol: {currentChar}")
-            tokens.append(Symbol(currentChar, position))
+            symbolTokens.append(Symbol(currentChar, rowIndex, position))
         elif not (currentChar in SYMBOLS_TO_IGNORE):
             raise Exception(f"Unrecognized character: {currentChar}")
         
         position += 1
 
     # print(f"Tokens processed: {tokens}")
-    return tokens
 
 def PrintTokens(tokens):
     for token in tokens:
         print(token.GetTokenValue())
 
 def ParseInput():
-    tokens = []
+    symbolTokens = []
+    numberTokens = []
     with open(FILENAME) as file:
-        for row in file:
+        for rowIndex, row in enumerate(file):
             row = row.strip("\n")
-            for token in CreateTokens(row):
-                tokens.append(token)
+            CreateTokens(rowIndex, row, symbolTokens, numberTokens)
                     
-    # PrintTokens(tokens)
-    return tokens
+    PrintTokens(symbolTokens)
+    PrintTokens(numberTokens)
+    return symbolTokens, symbolTokens
                         
 if __name__ == "__main__":
-    tokens = ParseInput()
+    symbolTokens, symbolTokens = ParseInput()
